50d4af68c86cb580b86953827a294c9791193d99
kzwkt <9437935+kzwkt@users.noreply.github.com>
2019-03-18 15:09:35 +0545

    Update alphanumupplow.py

--- alphanumupplow.py
+++ alphanumupplow.py
@@ -1,21 +1,21 @@
 if __name__ == '__main__':
     s = input()
     al=up=low=num=0
-    if s.isalnum():
-        print(True)
-    else:
-        print('False\n'*5)
 
     for a in s:
         if a.isalpha():
             al+=1
-            if a.isupper():
-                up+=1
-            if a.islower():
-                low+=1
+        if a.isupper():
+            up+=1
+        if a.islower():
+            low+=1
         if a.isdigit():
             num+=1
-    arr=[al,num,low,up]     
+    arr=[al,num,low,up]    
+    if(num>0 or al>0) :
+        print(True) 
+    else:
+        print(False) 
     for x in arr:
         if x>0:
              print(True) 
